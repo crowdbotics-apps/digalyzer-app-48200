8971932e1c1e7f054467e82413d21e1ca1cc1dff
Crowdbotics <team@crowdbotics.com>
2024-05-31 11:52:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digalyzer-app-48200",
+  url: "https://digalyzer-app-48200.botics.co",
+  api: "https://digalyzer-app-48200.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
